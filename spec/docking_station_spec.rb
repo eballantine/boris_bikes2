28d4a46ee423548bc98e59f9af9838c20e7c0931
eballantine <lizzy.kayballantine@gmail.com>
2021-07-29 14:23:01 +0100

    Refactor

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,14 +8,20 @@ describe DockingStation do
     expect { docking_station.release_bike }.to raise_error "No bikes available"
   end
 
-  it 'checks docking station is functional' do
+  it 'can tell you whether it has bikes' do
     expect(docking_station.has_bikes?).to eq false
+  end
+
+  it 'can release a bike' do
     expect(docking_station).to respond_to(:release_bike)
+  end
+
+  it 'can tell you if a bike is working' do
     expect(bike1.working?).to eq true
-    expect(docking_station).to respond_to(:dock)
   end
   
-  it 'checks docking a bike works' do
+  it 'can dock a bike' do
+    expect(docking_station).to respond_to(:dock)
     docking_station.dock(bike1)
     expect(docking_station).to have_attributes(:bikes => [bike1])
     expect(docking_station.has_bikes?).to eq true
